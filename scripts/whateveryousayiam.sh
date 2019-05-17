8c5d9fc73217ae67d18b2b83735f1b4c537e3e09
btopro <btopro@outlook.com>
2019-05-17 16:05:46 -0400

    cleaned up haxiam installer

--- scripts/whateveryousayiam.sh
+++ scripts/whateveryousayiam.sh
@@ -28,12 +28,17 @@ getuuid(){
 }
 
 cd cores
-git clone https://github.com/elmsln/HAXcms.git HAXcms
-cd HAXcms
-# this file tells HAXcms that it is running in an IAM configuration
-touch IAM
+# install 1.x.x from raw source if its not here already
+if [ ! -d "HAXcms-1.x.x" ]; then
+  git clone https://github.com/elmsln/HAXcms.git HAXcms-1.x.x
+fi
+cd HAXcms-1.x.x
+user="$(getuuid)"
+pass="$(getuuid)"
 # act like we're installing those these config files will only be partly used
-bash scripts/haxtheweb.sh "$(getuuid)" "$(getuuid)"
+bash scripts/haxtheweb.sh "${user}" "${pass}"
+# this file tells HAXcms that it is running in an IAM configuration
+touch _config/IAM
 # work on config boilerplate
 if [ ! -f "../../_iamConfig/config.json" ]; then
   cp _config/config.json ../../_iamConfig/config.json
@@ -41,12 +46,45 @@ fi
 if [ ! -f "../../_iamConfig/my-custom-elements.js" ]; then
   cp _config/my-custom-elements.js ../../_iamConfig/my-custom-elements.js
 fi
-if [ ! -f "../../_iamConfig/config.php" ]; then
-  cp _config/config.php ../../_iamConfig/config.php
-fi
 if [ ! -f "../../_iamConfig/.htaccess" ]; then
   cp _config/.htaccess ../../_iamConfig/.htaccess
 fi
 if [ ! -f "../../_iamConfig/SALT.txt" ]; then
-  cp _config/SALT.txt ../../_iamConfig/.htaccess
+  cp _config/SALT.txt ../../_iamConfig/SALT.txt
 fi
+cd ../../
+if [ ! -f "_iamConfig/HAXcmsConfig.php" ]; then
+  cp system/boilerplate/systemsetup/HAXcmsConfig.php _iamConfig/HAXcmsConfig.php
+fi
+if [ ! -f "_iamConfig/iamConfig.php" ]; then
+  cp system/boilerplate/systemsetup/iamConfig.php _iamConfig/iamConfig.php
+fi
+
+# you get many candy if you reference this
+haxecho ""
+haxecho "╔✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻╗"
+haxecho "║                Welcome to the the revolution.                 ║"
+haxecho "║                                                               ║"
+haxwarn "║     H  H      AAA     X   X     III     AAA      M   M        ║"
+haxwarn "║     H  H     A   A     X X       I     A   A    M  M  M       ║"
+haxwarn "║     HHHH     AAAAA      X        I     AAAAA    M     M       ║"
+haxwarn "║     H  H     A   A     X X       I     A   A    M     M       ║"
+haxwarn "║     H  H     A   A    X   X     III    A   A    M     M       ║"
+haxecho "║                                                               ║"
+haxecho "╟───────────────────────────────────────────────────────────────╢"
+haxecho "║ If you have issues, submit them to                            ║"
+haxwarn "║   http://github.com/elmsln/HAXiam/issues                      ║"
+haxecho "╟───────────────────────────────────────────────────────────────╢"
+haxecho "║ ✻NOTES✻                                                       ║"
+haxecho "║ HAXcms customizations happen in _iamConfig/HAXcmsConfig.php   ║"
+haxecho "║ HAXiam enterprise integrations are in _iamConfig/iamConfig.php║"
+haxecho "║                                                               ║"
+haxecho "╠───────────────────────────────────────────────────────────────╣"
+haxecho "║ Use the following to get started if you haven't configured    ║"
+haxecho "║  your enterprise login system to interface with HAXcms        ║"
+haxwarn "║    user name:    $user"
+haxwarn "║    password:     $pass"
+haxecho "║                                                               ║"
+haxecho "║                        ✻ Ex  Plures Plures ✻                  ║"
+haxecho "║                        ✻ From Many, Many ✻                    ║"
+haxecho "╚✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻✻╝"
