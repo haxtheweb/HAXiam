ffaf962f28463f32df7386d66324e2a11b33ca88
btopro <btopro@outlook.com>
2020-01-09 13:00:39 -0500

    login script has correct path / setup now

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php
+define("IAM_INTERNALS", "login");
 include_once 'system/lib/IAM.php';
-include_once IAM_ROOT . '/_iamConfig/iamConfig.php';
+include_once IAM_ROOT . '/cores/' . HAXIAM_ACTIVE_CORE . '/system/lib/bootstrapHAX.php';
+include_once $HAXCMS->configDirectory . '/config.php';
 if (isset($IAM->enterprise->userVar)) {
 	// execute setting up the IAM wrapper
-	setcookie('haxcms_refresh_token', $GLOBALS['HAXCMS']->getRefreshToken($IAM->enterprise->userVar), $_expires = 0, $_path = '/', $_domain = '', $_secure = false, $_httponly = true);
+	setcookie('haxcms_refresh_token', $HAXCMS->getRefreshToken($IAM->enterprise->userVar), $_expires = 0, $_path = '/', $_domain = '', $_secure = false, $_httponly = true);
 	header("Location: " . $IAM->enterprise->iamUrl);
 }
 else {
