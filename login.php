e19339f850f587a8db4a9f114577d0d4c413d98c
btopro <btopro@outlook.com>
2020-01-09 11:06:13 -0500

    setcookie bc login is happening at a global level

--- login.php
+++ login.php
@@ -3,6 +3,7 @@ include_once 'system/lib/IAM.php';
 include_once IAM_ROOT . '/_iamConfig/iamConfig.php';
 if (isset($IAM->enterprise->userVar)) {
 	// execute setting up the IAM wrapper
+	setcookie('haxcms_refresh_token', $GLOBALS['HAXCMS']->getRefreshToken($IAM->enterprise->userVar), $_expires = 0, $_path = '/', $_domain = '', $_secure = false, $_httponly = true);
 	header("Location: " . $IAM->enterprise->iamUrl);
 }
 else {
