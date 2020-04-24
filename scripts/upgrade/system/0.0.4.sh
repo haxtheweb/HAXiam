0bd6c3b4c5651d81961a2c3f0f57cfe7def4cde5
btopro <btopro@outlook.com>
2020-04-24 13:32:23 -0400

    users_sites fix

--- scripts/upgrade/system/0.0.4.sh
+++ scripts/upgrade/system/0.0.4.sh
@@ -3,9 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../../../
 source _iamConfig/config.cfg
-# loop through users and ensure there's a symlink for these new files
-for i in $(find $haxiam/users -maxdepth 1 -type d); do
-  if [[ "${haxiam}/users" != "${i}" ]]; then
+# loop through user sites linkage and ensure there's a symlink for these new files
+for i in $(find $haxiam/users_sites -maxdepth 1 -type d); do
+  if [[ "${haxiam}/users_sites" != "${i}" ]]; then
     cd $i
     ln -s "../../cores/${haxcmscore}/build-legacy.js" build-legacy.js
     ln -s "../../cores/${haxcmscore}/build-haxcms.js" build-haxcms.js
