7696c40ea6580d8c3c0f41d011d9a86f79b654b8
btopro <btopro@outlook.com>
2021-01-08 15:00:02 -0500

    path rel

--- system/3rdparty/gitlist/boot.php
+++ system/3rdparty/gitlist/boot.php
@@ -1,1 +1,1 @@
-../../cores/HAXcms-1.x.x/system/backend/php/3rdparty/gitlist/boot.php
+../../../cores/HAXcms-1.x.x/system/backend/php/3rdparty/gitlist/boot.php
--- system/3rdparty/gitlist/index.php
+++ system/3rdparty/gitlist/index.php
@@ -1,1 +1,1 @@
-../../cores/HAXcms-1.x.x/system/backend/php/3rdparty/gitlist/index.php
+../../../cores/HAXcms-1.x.x/system/backend/php/3rdparty/gitlist/index.php
