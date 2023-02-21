f8f5bf874a1c800fdfe16835feb804ece86c1066
btopro <btopro@outlook.com>
2023-02-21 16:07:08 -0500

    logic hole found by student

--- login.php
+++ login.php
@@ -1,12 +1,17 @@
 <?php
 define("IAM_INTERNALS", "login");
 include_once 'system/lib/IAM.php';
+include_once IAM_ROOT . '/_iamConfig/iamConfig.php';
 include_once IAM_ROOT . '/cores/' . HAXIAM_ACTIVE_CORE . '/system/backend/php/bootstrapHAX.php';
 include_once $HAXCMS->configDirectory . '/config.php';
 if (isset($IAM->enterprise->userVar)) {
 	// execute setting up the IAM wrapper
 	setcookie('haxcms_refresh_token', $HAXCMS->getRefreshToken($IAM->enterprise->userVar), $_expires = 0, $_path = '/', $_domain = '', $_secure = false, $_httponly = true);
-	header("Location: " . $IAM->enterprise->iamUrl . $IAM->enterprise->userVar);
+	// verify they have a user directory
+  if (!is_dir(IAM_ROOT . '/users/' . $IAM->enterprise->userVar)) {
+    $IAM->liberate($IAM->enterprise->userVar);
+  }
+  header("Location: " . $IAM->enterprise->iamUrl . $IAM->enterprise->userVar);
 }
 else {
 	// do something to login
