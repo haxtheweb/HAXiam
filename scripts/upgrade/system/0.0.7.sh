84933068d96f6fd2859069fb1803c4324c403661
btopro <btopro@outlook.com>
2023-06-27 15:06:08 -0400

    https://github.com/elmsln/issues/issues/1355

---
+++ scripts/upgrade/system/0.0.7.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+cd ../../../
+source _iamConfig/config.cfg
+# check that we are the root user
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run as root"
+  exit 1
+fi
+# add in userData.json symlink cause we missed that one
+for i in $(find $haxiam/users -maxdepth 1 -type d); do
+  if [[ "${haxiam}/users" != "${i}" ]]; then
+    cd $i/_config
+    ln -s ../../../_iamConfig/userData.json userData.json
+  fi
+done
+
+# rebuild files for all sites to be performance optimized even in older stuff
+for i in $(find $haxiam/users -maxdepth 1 -type d); do
+  if [[ "${haxiam}/users" != "${i}" ]]; then
+    cd $i
+    haxcms rebuildManagedFiles __ALL__ $i
+    cd ..
+  fi
+done
