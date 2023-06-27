3266746888da843640bfd6abac3b9b9c6ae05b66
btopro <btopro@outlook.com>
2023-06-27 08:27:04 -0400

    lineendingfix

--- login.php
+++ login.php
@@ -8,10 +8,10 @@ if (isset($IAM->enterprise->userVar)) {
 	// execute setting up the IAM wrapper
 	setcookie('haxcms_refresh_token', $HAXCMS->getRefreshToken($IAM->enterprise->userVar), $_expires = 0, $_path = '/', $_domain = '', $_secure = false, $_httponly = true);
 	// verify they have a user directory
-  if (!is_dir(IAM_ROOT . '/users/' . $IAM->enterprise->userVar)) {
-    $IAM->liberate($IAM->enterprise->userVar);
-  }
-  header("Location: " . $IAM->enterprise->iamUrl . $IAM->enterprise->userVar);
+	if (!is_dir(IAM_ROOT . '/users/' . $IAM->enterprise->userVar)) {
+		$IAM->liberate($IAM->enterprise->userVar);
+	}
+	header("Location: " . $IAM->enterprise->iamUrl . $IAM->enterprise->userVar);
 }
 else {
 	// do something to login
